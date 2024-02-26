24f289f70d1a5098ae2591067c8d6a9497a2e41c
nourelhoudabmz <nourelhouda.iot@gmail.com>
2024-02-26 16:07:15 +0100

    User

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const voitureroute = require ('./routes/voitures')
 const bodyParser = require('body-parser');
-
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 const app = express();
 const port = 3000;
+
+dotenv.config()
+const Mongodb_URI= process.env.Mongodb_URI
+const PORT =process.env.port || 3000
+
+
+
 
 app.use(express.json());
 app.use('/voiture',voitureroute)
 app.use(bodyParser.json());
+
+//connection to mongodb and start server
+mongoose.connect(Mongodb_URI).then(()=>{
+  console.log('connected to Mongodb');
+
+}).catch((err)=>{
+  console.error('Erroe connecting to mongodb:',err.messag)
+})
 
 
 
---
+++ models/user.js
@@ -0,0 +1,9 @@
+const mongoose =require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema =new mongoose.Schema({
+    username:{type:String,unique:true},
+    password:String
+
+})
+
